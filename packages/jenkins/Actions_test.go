70db1dde6166d1325dad797bcb4d23c8f74ca3e5
Abel Gancsos <agancsos@users.noreply.github.com>
2022-05-16 23:52:15 -0400

    Update Actions_test.go

--- packages/jenkins/Actions_test.go
+++ packages/jenkins/Actions_test.go
@@ -2,7 +2,7 @@ package jenkins
 import "testing"
 
 func TestGetJobs(t *testing.T) {
-	var client      = &JenkinsClient{Username: "adminabel", PAT: "11086223c288eb4321b381740207f2142c"};
+	var client      = &JenkinsClient{Username: "", PAT: ""};
 	var jobs        = GetJobs(client, "http://jenkins:8080", []string{"workflowjob"}, []string{}, false);
 	if len(jobs) < 1 {
 		t.Fail();
